f74bef3ecd6383fb6b1e589109fc3da6b28af632
hayleyskyland <hwitherell@skylandanalytics.net>
2021-12-14 14:36:46 -0700

    Add i+1

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ $(function() {
   var para = $("p");
   var i = 0;
   
-  para.text(i);
+  para.text(i+1);
 
   function toggleBox(i) {
     box.fadeToggle(500, function() {
       i = i + 1;
 
       if(i < 10) {
-        para.text(i);
+        para.text(i+1);
         toggleBox(i);
       };
     });
