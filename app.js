2818ae1a6a4888447318bf5d68a0c8f8db8309dd
hayleyskyland <hwitherell@skylandanalytics.net>
2021-12-14 14:33:58 -0700

    Add toggleBox()

--- app.js
+++ app.js
@@ -4,4 +4,19 @@ $(function() {
   var box = $("#box");
   var para = $("p");
   var i = 0;
+  
+  para.text(i);
+
+  function toggleBox(i) {
+    box.fadeToggle(500, function() {
+      i = i + 1;
+
+      if(i < 10) {
+        para.text(i);
+        toggleBox(i);
+      };
+    });
+  };
+  
+  toggleBox(i);
 });
