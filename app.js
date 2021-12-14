933e55108a7365be33b67ad92ae9a3629f610285
hayleyskyland <hwitherell@skylandanalytics.net>
2021-12-14 15:23:09 -0700

    Start traversing page

--- app.js
+++ app.js
@@ -8,21 +8,27 @@ $(function() {
   para.text(i+1);
 
   function toggleBox(i) {
-    box.fadeToggle(1000, function() {
-      i = i + 1;
+    box.fadeToggle(1500, function() {
+      i = i + 1;   
 
-      if(i < 10) {
+      if(i < 4) {
         para.text(i+1);
         toggleBox(i);
-      };
+      }
     });
   };
+
+  // `.eq(#)` references one of the test `<h3>`s as if an array
+
+  alert($("h3").eq(1).text());
   
   toggleBox(i);
 });
 
+// this changes color of `<p>` to use `paragraph` css section
+
 $("p").addClass("paragraph");
 
-$("div>ul a");
-$("div#main p strong");
-$("div.main p>li a");
+// $("div>ul a");
+// $("div#main p strong");
+// $("div.main p>li a");
