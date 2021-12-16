c0a0b54758113022b022909c77899c4fdc35f6f2
hayleyskyland <hwitherell@skylandanalytics.net>
2021-12-16 12:26:10 -0700

    Refactor functions and comments

--- app.js
+++ app.js
@@ -2,19 +2,24 @@
 
 $(function() {
 
-  // target html items by id
 
-  var box = $('#box');
-  var counter = $('#counter');
-  var traversal = $('#traversal')
 
-  // toggle box counter
 
-  var i = 0;
-  
+  //////////////////// TARGET HTML ITEMS BY ID ////////////////////
+
+  const box = $('#box');
+  const counter = $('#counter');
+  const traversal = $('#traversal')
+
+
+
+
+  //////////////////// BOX COUNTER ////////////////////
+
+  let i = 0;
   counter.text(i+1);
 
-  function toggleBox(i) {
+  const toggleBox = (i) => {
     box.fadeToggle(1500, function() {
       i = i + 1;   
 
@@ -25,78 +30,92 @@ $(function() {
     });
   };
 
-  // traverse A-C
-    // `.eq(#)` references one of the test `<h3>`s as if an array
 
-  function traverse() {
 
-    // log A-C (but not Z)
 
-    let countUp = traversal.children()
+  //////////////////// TRAVERSAL CONSOLE LOGS ////////////////////
 
-    for (let i = 0; i < countUp.length; i++) {
-      console.log('count up:', countUp.eq(i).text());
+
+  function countingUp() {   
+
+    const countWithoutZ = () => {
+      let countThis = traversal.children();
+
+      for (let i = 0; i < countThis.length; i++) {
+        console.log('count up line 1:', countThis.eq(i).text());
+      }
     }
 
-    // log info about 1st (A) & last (C) letter
+    const countWithZ = () => {
+      let countWithZ = $('h3');
+      console.log('count up line 2:', countWithZ.eq(3).text());
+    }
 
-    // console.log('first letter info:', $('h3:first'))
-    // console.log('last letter info:', $('h3:last'))
+    countWithoutZ();
+    countWithZ();
+  }
 
-    let articleLog = $('article h3')
-    let traverseLog = traversal.children('h3')
-    let siblingLog = $('h3').siblings()
-    let nextLog = $('h3:first').nextAll()
+
+  const logFirstAndLastLetters = () => {
+    console.log('first letter:', $('h3:first').text())
+    console.log('last letter :', $('h3:last').text())
+  }
+
+
+  function logChildrenAndSiblings() {
+
+    let articleLog = $('article h3').text()
+    let traverseLog = traversal.children('h3').text()
+    let nextLog = $('h3:first').nextAll().andSelf().text()
     let findLog = (traversal).find('h3')
-
-    //target just inside `traversal` article
+    // let siblingLog = $('h3').siblings().text()
 
     // console.log('all letter info via `article h3`', articleLog)
     // console.log('all letter info via `children`:', traverseLog)
-    // console.log('all letter info via `siblings`:', siblingLog)
     // console.log('all letter info via `next`:', nextLog)
-
-    // supposed to target whole dom, but it's just targetting `traversal` article
-    // console.log('all letter info via `find`:', findLog) 
-
-    if (siblingLog = nextLog) {
-      console.log('success: traversing siblings & next both match')
-    }
+    // console.log('all letter info via `siblings`:', siblingLog)    
     
-    if (
-      (articleLog = traverseLog)
-      && (articleLog = siblingLog.andSelf())
-      && (articleLog = nextLog.andSelf())
-      && (articleLog = findLog)
-    ) {
-      console.log('success: all traversal logs are the same')
+    const allLogsMatch = () => {
+      if (
+        (articleLog = traverseLog)
+        // && (articleLog = siblingLog)
+        && (articleLog = nextLog)
+        && (articleLog = findLog)
+      ) {
+        console.log('success: all logs match')
+      }
     }
 
-    // this one is supposed to search whole dom, but it's not incorrectly showing up same result
-
-    let findBody = $('#body').find('h3')
-
-    // console.log(findBody)
-
-    if (findLog = findBody) {
-      console.log('fail: failed to search body vs article')
-    }      
-
-    // search whole dom for h3s
-    // console.log('all letter info via `find`:', findLog)    
+    allLogsMatch();
   }
-
+  
   // invoke functions
 
   toggleBox(i);
-  traverse();  
+
+  console.log('***** COUNT UP *****')
+  countingUp();
+
+  console.log('***** LOG FIRST & LAST LETTERS *****')  
+  logFirstAndLastLetters();
+
+  console.log('***** LOG CHILDREN & SIBLINGS *****')   
+  logChildrenAndSiblings();
 });
+
+
+
+
+//////////////////// CHANGE CSS ////////////////////
 
 // add a class to an html element (to add color in css)
 
 $(counter).addClass('colorize-counter');
 
-// notes of how to reference html in different ways
+
+
+
+//////////////////// NOTES ////////////////////
 
 // $('div>ul a');
 // $('div#main p strong');
