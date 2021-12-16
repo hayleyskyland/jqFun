2b1446861f73aae37b39bc7d99771cf8b6c5fe91
hayleyskyland <hwitherell@skylandanalytics.net>
2021-12-16 10:43:17 -0700

    Organize different pratices

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 // this 1st function is invoked on load, basically a build-in `onReady`
 
 $(function() {
-  var box = $("#box");
-  var para = $("p");
+  var box = $('#box');
+  var counter = $('#counter');
   var i = 0;
   
-  para.text(i+1);
+  counter.text(i+1);
 
   function toggleBox(i) {
     box.fadeToggle(1500, function() {
       i = i + 1;   
 
       if(i < 4) {
-        para.text(i+1);
+        counter.text(i+1);
         toggleBox(i);
       }
     });
@@ -20,15 +20,20 @@ $(function() {
 
   // `.eq(#)` references one of the test `<h3>`s as if an array
 
-  alert($("h3").eq(1).text());
-  
+  function traverse() {
+    console.log($('h3').eq(0).text());
+    console.log($('h3').eq(1).text());
+    console.log($('h3').eq(2).text());
+  }
+
   toggleBox(i);
+  traverse();  
 });
 
 // this changes color of `<p>` to use `paragraph` css section
 
-$("p").addClass("paragraph");
+$('p').addClass('paragraph');
 
-// $("div>ul a");
-// $("div#main p strong");
-// $("div.main p>li a");
+// $('div>ul a');
+// $('div#main p strong');
+// $('div.main p>li a');
