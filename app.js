e87cdb232e3d1df2ec699c6943256ccf15ddb153
hayleyskyland <hwitherell@skylandanalytics.net>
2021-12-16 11:40:33 -0700

    Play with counting

--- app.js
+++ app.js
@@ -30,10 +30,12 @@ $(function() {
 
   function traverse() {
 
-    // log all 3 letters
+    // log A-C (but not Z)
 
-    for (let i = 0; i < $('h3').length; i++) {
-      console.log('count up:', $('h3').eq(i).text());
+    let countUp = traversal.children()
+
+    for (let i = 0; i < countUp.length; i++) {
+      console.log('count up:', countUp.eq(i).text());
     }
 
     // log info about 1st (A) & last (C) letter
@@ -42,28 +44,42 @@ $(function() {
     // console.log('last letter info:', $('h3:last'))
 
     let articleLog = $('article h3')
-    let traverseLog = (traversal).children('h3')
-    let siblingLog = $('h3').siblings().andSelf() 
+    let traverseLog = traversal.children('h3')
+    let siblingLog = $('h3').siblings()
+    let nextLog = $('h3:first').nextAll()
     let findLog = (traversal).find('h3')
-    let findBody = $('#body').find('h3')
 
     //target just inside `traversal` article
 
     // console.log('all letter info via `article h3`', articleLog)
     // console.log('all letter info via `children`:', traverseLog)
     // console.log('all letter info via `siblings`:', siblingLog)
+    // console.log('all letter info via `next`:', nextLog)
 
     // supposed to target whole dom, but it's just targetting `traversal` article
     // console.log('all letter info via `find`:', findLog) 
 
-    // ^^ this one searches whole dom though? incorrectly showing up same result
-
-    if ((articleLog = traverseLog) && (articleLog = siblingLog) && (articleLog = findLog)) {
-      console.log('all traversal logs are the same')
+    if (siblingLog = nextLog) {
+      console.log('success: traversing siblings & next both match')
+    }
+    
+    if (
+      (articleLog = traverseLog)
+      && (articleLog = siblingLog.andSelf())
+      && (articleLog = nextLog.andSelf())
+      && (articleLog = findLog)
+    ) {
+      console.log('success: all traversal logs are the same')
     }
 
+    // this one is supposed to search whole dom, but it's not incorrectly showing up same result
+
+    let findBody = $('#body').find('h3')
+
+    // console.log(findBody)
+
     if (findLog = findBody) {
-      console.log('fail')
+      console.log('fail: failed to search body vs article')
     }      
 
     // search whole dom for h3s
