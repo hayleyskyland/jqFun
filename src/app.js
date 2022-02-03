a3b6008196107663e740560e9c07d557cfd031f4
hayleyskyland <hwitherell@skylandanalytics.net>
2022-02-03 12:32:03 -0700

    Switch to double quotes for ajax

--- src/app.js
+++ src/app.js
@@ -1,36 +1,36 @@
 //////////////////// TARGET HTML ITEMS BY ID ////////////////////
 
-const body = $('body');
-const title = $('h1');
-const box = $('#box');
-const counter = $('#counter');
-const traversal = $('#traversal');
-const h3 = $('h3');
+const body = $("body");
+const title = $("h1");
+const box = $("#box");
+const counter = $("#counter");
+const traversal = $("#traversal");
+const h3 = $("h3");
 
 // buttons
 
-const orangeBtn = $('#orangeBtn');
-const greenBtn = $('#greenBtn');
-const pinkBtn = $('#pinkBtn');
+const orangeBtn = $("#orangeBtn");
+const greenBtn = $("#greenBtn");
+const pinkBtn = $("#pinkBtn");
 
-const fogBtn = $('#fogBtn');
-const unfogBtn = $('#unfogBtn');
+const fogBtn = $("#fogBtn");
+const unfogBtn = $("#unfogBtn");
 
-const sunBtn = $('#dayLink')
-const moonBtn = $('#nightLink');
+const sunBtn = $("#dayLink")
+const moonBtn = $("#nightLink");
 
 // accordion
 
-const accordionHeading = $('h4');
+const accordionHeading = $("h4");
 
-const iconExpandCollapse = $('.icon-expand-collapse');
+const iconExpandCollapse = $(".icon-expand-collapse");
 
-const collapseIcon = [$('#collapse0'), $('#collapse1'), $('#collapse2')];
-const sectionText = [$('#sectionText0'), $('#sectionText1'), $('#sectionText2')];
-const expandIcon = [$('#expand0'), $('#expand1'), $('#expand2')];
+const collapseIcon = [$("#collapse0"), $("#collapse1"), $("#collapse2")];
+const sectionText = [$("#sectionText0"), $("#sectionText1"), $("#sectionText2")];
+const expandIcon = [$("#expand0"), $("#expand1"), $("#expand2")];
 
-const expandAll = $('#expandAll');
-const collapseAll = $('#collapseAll');
+const expandAll = $("#expandAll");
+const collapseAll = $("#collapseAll");
 
 
 
@@ -38,19 +38,19 @@ const collapseAll = $('#collapseAll');
 
 const hide = (elements) => {
   elements.forEach(element => {
-    element.addClass('hidden');
+    element.addClass("hidden");
   });
 };
 
 const show = (elements) => {
   elements.forEach(element => {
-    element.removeClass('hidden');
+    element.removeClass("hidden");
   });
 };
 
 const setColor = (elements, colorVar) => {
   elements.forEach(element => {
-    element.css('color', colorVar)
+    element.css("color", colorVar)
   });
 };
 
@@ -59,16 +59,16 @@ const setColor = (elements, colorVar) => {
 //////////////////// THEME ////////////////////
 
 const setTheme = (color) => {
-  body.css('background', color);
+  body.css("background", color);
   setColor([title, fogBtn, unfogBtn, box, accordionHeading, iconExpandCollapse], color);
-  $.cookie('theme', color, { expires: 7 });
+  $.cookie("theme", color, { "expires": 7 });
 
-  if (color === 'white') {
-    setColor([body], 'gray');
+  if (color === "white") {
+    setColor([body], "gray");
     hide([sunBtn]);
     show([moonBtn]);
 
-  } else if (color === 'black') {
+  } else if (color === "black") {
     hide([moonBtn]);
     show([sunBtn]);
   };
@@ -79,23 +79,23 @@ $(function() {
   hide([sunBtn])
 
   $(function() {
-    sunBtn.on('click', function(event) {
+    sunBtn.on("click", function(event) {
       event.preventDefault();
-      setTheme('white');
+      setTheme("white");
     });
   });
 
   $(function() {
-    moonBtn.on('click', function(event) {
+    moonBtn.on("click", function(event) {
       event.preventDefault();
-      setTheme('black');
+      setTheme("black");
     });
   });
 });
 
 $(function() {
-  if ($.cookie('theme') === 'black') {
-    setTheme('black');
+  if ($.cookie("theme") === "black") {
+    setTheme("black");
   };
 });
 
@@ -107,14 +107,14 @@ $(function() {
 
 $(function() {
   let i = 0;
-  counter.text('fadetoggle: ' + (i+1))
+  counter.text("fadetoggle: " + (i+1))
 
   const toggleBox = (i) => {
     box.fadeToggle(1500, function() {
       i = i + 1;   
 
       if(i < 4) {
-        counter.text('fadetoggles: ' + (i+1));
+        counter.text("fadetoggles: " + (i+1));
         toggleBox(i)
       };
     });
@@ -126,7 +126,7 @@ $(function() {
 // coordinates
 
 $(function() {
-  box.on('click', function(event) {
+  box.on("click", function(event) {
     alert(`Your mouse is at X ${event.pageX} & Y ${event.pageY}.`)
   });
 });
@@ -139,23 +139,23 @@ $(function() {
 
   // create logs
 
-  const h3First = $('h3:first');
-  const h3Last = $('h3:last');
+  const h3First = $("h3:first");
+  const h3Last = $("h3:last");
 
-  const countUp = () => {   
+  const countUp = () => {
 
     const countWithoutZ = () => {
       let countThis = traversal.children();
 
       for (let i = 0; i < countThis.length; i++) {
-        console.log('count up line 1:', countThis.eq(i).text());
+        console.log("count up line 1:", countThis.eq(i).text());
       };
     };
 
     const countWithZ = () => {
-      let countWithZ = $('h3');
+      let countWithZ = $("h3");
 
-      console.log('count up line 2:', countWithZ.eq(3).text());
+      console.log("count up line 2:", countWithZ.eq(3).text());
     }
 
     countWithoutZ();
@@ -163,12 +163,12 @@ $(function() {
   };
 
   const logFirstAndLastLetters = () => {
-    console.log('first letter:', h3First.text())
-    console.log('last letter :', h3Last.text())
+    console.log("first letter:", h3First.text())
+    console.log("last letter :", h3Last.text())
   };
 
   const logChildrenAndSiblings = () => {
-    let articleLog = $('article h3').text();
+    let articleLog = $("article h3").text();
     let traverseLog = traversal.children(h3).text();
     let nextLog = h3First.nextAll().andSelf().text();
     let findLog = (traversal).find(h3);
@@ -182,30 +182,30 @@ $(function() {
           && (articleLog = findLog)
           && (articleLog = siblingLog)
         ) {
-          return 'all logs match';
+          return "all logs match";
         };
       };
 
-      console.log('log line 1:', articleLog, '-', allLogsMatch())
+      console.log("log line 1:", articleLog, "-", allLogsMatch())
     });
 
     $(function() {
       let articleLog = h3.text();
 
-      console.log('log lines 1 & 2:', articleLog)
+      console.log("log lines 1 & 2:", articleLog)
     });
   };
 
   // invoke logs
 
   $(function() {
-    console.log('***** COUNT UP *****');
+    console.log("***** COUNT UP *****");
     countUp();
 
-    console.log('***** FIRST & LAST LETTERS *****');
+    console.log("***** FIRST & LAST LETTERS *****");
     logFirstAndLastLetters();
 
-    console.log('***** CHILDREN & SIBLINGS *****');
+    console.log("***** CHILDREN & SIBLINGS *****");
     logChildrenAndSiblings();
   });
 });
@@ -225,10 +225,10 @@ $(function() {
   $(function(){
     const hoverHere = (elements) => {
       elements.forEach(btn => {
-        btn.on('mouseenter', function() {
-          btn.css('transform', 'scale(1.5)');
-        }).on('mouseleave', function() {
-          btn.css('transform', 'none');
+        btn.on("mouseenter", function() {
+          btn.css("transform", "scale(1.5)");
+        }).on("mouseleave", function() {
+          btn.css("transform", "none");
         });
       });
     };
@@ -240,7 +240,7 @@ $(function() {
 
   $(function(){
     orangeBtn.click(function() {
-      h3.css('color', 'orange');
+      h3.css("color", "orange");
       hide([orangeBtn]);
       show([greenBtn, pinkBtn]);
     });
@@ -248,7 +248,7 @@ $(function() {
 
   $(function(){
     pinkBtn.click(function() {
-      setColor([h3], 'lightpink');
+      setColor([h3], "lightpink");
       hide([pinkBtn]);
       show([greenBtn, orangeBtn]);
     });
@@ -256,7 +256,7 @@ $(function() {
 
   $(function(){
     greenBtn.click(function() {
-      setColor([h3], 'olivedrab');
+      setColor([h3], "olivedrab");
       hide([greenBtn]);
       show([pinkBtn, orangeBtn]);
     });
@@ -277,16 +277,16 @@ $(function() {
   const animateFogBtn = (elements) => {
     elements.forEach(btn => {
       // grow 1
-      btn.animate({ 'width': 200 }, 4000)
-      .animate({ 'width' : 175 })
+      btn.animate({ "width": 200 }, 4000)
+      .animate({ "width": 175 })
       // grow 2
-      .animate({ 'width': 200 }, 1000)
-      .animate({ 'width' : 100 }, 1000)
+      .animate({ "width": 200 }, 1000)
+      .animate({ "width": 100 }, 1000)
       // grow 3
-      .animate({ 'width': 200 }, 3000)
-      .animate({ 'width' : 175 })
+      .animate({ "width": 200 }, 3000)
+      .animate({ "width": 175 })
       // grow 4
-      .animate({'width': 200}, 1000)
+      .animate({ "width": 200}, 1000)
     });
   };
 
@@ -296,7 +296,7 @@ $(function() {
 
   $(function(){
     fogBtn.click(function() {
-      body.addClass('fog');
+      body.addClass("fog");
       hide([fogBtn]);
       show([unfogBtn]);
     });
@@ -304,7 +304,7 @@ $(function() {
 
   $(function(){
     unfogBtn.click(function() {
-      body.removeClass('fog');
+      body.removeClass("fog");
       show([fogBtn]);
       hide([unfogBtn]);
     });
@@ -320,35 +320,35 @@ $(function() {
 
   // click text above paragraphs
 
-  counter.on('click', function(event) {
-    alert('This line counts how many times the above box fades.');
+  counter.on("click", function(event) {
+    alert("This line counts how many times the above box fades.");
   });
 
   
-  h3.on('click', function() {
-    alert('Check console to see examples of traversing the dom.');
+  h3.on("click", function() {
+    alert("Check console to see examples of traversing the dom.");
   });
 
   // click paragraphs
 
-  $('#p1').on('click', function(event) {
-    alert('You clicked paragraph 1.');
+  $("#p1").on("click", function(event) {
+    alert("You clicked paragraph 1.");
     event.stopPropagation();
   });
 
-  $('p').on('click', 'p', function() {
-    alert('stopPropagation should prevent this from displaying.');
+  $("p").on("click", "p", function() {
+    alert("stopPropagation should prevent this from displaying.");
   });
 
   // add paragraph
 
-  $(('<p />'), {
-    text: 'paragraph3 (click)',
-    id: 'p3'
-  }).appendTo('#paragraphs');
+  $(("<p />"), {
+    text: "paragraph3 (click)",
+    id: "p3"
+  }).appendTo("#paragraphs");
 
-  $('p:nth-child(3)').on('click', function() {
-    alert('You clicked paragraph 3.');
+  $("p:nth-child(3)").on("click", function() {
+    alert("You clicked paragraph 3.");
   });
 
 }); 
@@ -361,11 +361,11 @@ $(function() {
 
 const setAccordion = (accordion, status, num, clicked) => {
 
-  if (clicked === 'clicked') {
-    $.cookie(accordion, status, { expires: 7 });
+  if (clicked === "clicked") {
+    $.cookie(accordion, status, { "expires": 7 });
   }
 
-  if (status === 'close') {
+  if (status === "close") {
     hide([sectionText[num], collapseIcon[num]]);
     show([expandIcon[num]]);
   } else {
@@ -380,19 +380,19 @@ const setAccordion = (accordion, status, num, clicked) => {
 $(function() {
 
   const setAllSections = (status) => {
-    setAccordion('accordion0', status, 0, 'clicked');
-    setAccordion('accordion1', status, 1, 'clicked');
-    setAccordion('accordion2', status, 2, 'clicked');
+    setAccordion("accordion0", status, 0, "clicked");
+    setAccordion("accordion1", status, 1, "clicked");
+    setAccordion("accordion2", status, 2, "clicked");
   }
 
   collapseExpandAll = (btn, status) => {
-    btn.on('click', function() {
+    btn.on("click", function() {
       setAllSections(status);
     });
   }
 
-  collapseExpandAll(collapseAll, 'close')
-  collapseExpandAll(expandAll, 'open')
+  collapseExpandAll(collapseAll, "close")
+  collapseExpandAll(expandAll, "open")
 })
 
 // set one section
@@ -400,19 +400,19 @@ $(function() {
 $(function() {
 
   const setOneSection = (accordion, btn, status, num) => {
-    btn[num].on('click', function() {
-      setAccordion(accordion, status, num, 'clicked');
+    btn[num].on("click", function() {
+      setAccordion(accordion, status, num, "clicked");
     })
   }
 
-  setOneSection('accordion0', collapseIcon, 'close', 0)
-  setOneSection('accordion0', expandIcon, 'open', 0)
+  setOneSection("accordion0", collapseIcon, "close", 0)
+  setOneSection("accordion0", expandIcon, "open", 0)
 
-  setOneSection('accordion1', collapseIcon, 'close', 1)
-  setOneSection('accordion1', expandIcon, 'open', 1)
+  setOneSection("accordion1", collapseIcon, "close", 1)
+  setOneSection("accordion1", expandIcon, "open", 1)
 
-  setOneSection('accordion2', collapseIcon, 'close', 2)
-  setOneSection('accordion2', expandIcon, 'open', 2)
+  setOneSection("accordion2", collapseIcon, "close", 2)
+  setOneSection("accordion2", expandIcon, "open", 2)
 
 })
 
@@ -421,15 +421,15 @@ $(function() {
 $(function() {
 
   accordionCookieCall = (accordion, num) => {
-    if ($.cookie(accordion) == 'close') {
-      setAccordion(accordion, 'close', num);
+    if ($.cookie(accordion) == "close") {
+      setAccordion(accordion, "close", num);
     } else {
-      setAccordion(accordion, 'open', num);
+      setAccordion(accordion, "open", num);
     }
   }
 
-  accordionCookieCall('accordion0', 0)
-  accordionCookieCall('accordion1', 1)
-  accordionCookieCall('accordion2', 2)
+  accordionCookieCall("accordion0", 0)
+  accordionCookieCall("accordion1", 1)
+  accordionCookieCall("accordion2", 2)
 
 });
